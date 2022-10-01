8a11f716503442b976f61033b1f855e7d04a75c8
Matt W <matt.weale@outlook.com>
2022-10-01 11:25:48 +0100

    Updated provider block in main.tf

--- main/main.tf
+++ main/main.tf
@@ -9,12 +9,12 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
-}
-backend "azurerm" {
-  resource_group_name  = "rg-persistent"
-  storage_account_name = "samrwtfstate"
-  container_name       = "tfstate"
-  key                  = "aqua.terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "rg-persistent"
+    storage_account_name = "samrwtfstate"
+    container_name       = "tfstate"
+    key                  = "aqua.terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features {
