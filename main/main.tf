20ec2048cc6b703988bae221599ccb137f09c5ba
Matt W <matt.weale@outlook.com>
2022-10-01 11:20:17 +0100

    Fixed formatting

--- main/main.tf
+++ main/main.tf
@@ -8,14 +8,15 @@ terraform {
       version = "~> 3.1.1"
     }
   }
+}
 
-  required_version = ">= 1.3.0"
-  backend "azurerm" {
-    resource_group_name  = "rg-persistent"
-    storage_account_name = "samrwtfstate"
-    container_name       = "tfstate"
-    key                  = "aqua.terraform.tfstate"
-  }
+required_version = ">= 1.3.0"
+backend "azurerm" {
+  resource_group_name  = "rg-persistent"
+  storage_account_name = "samrwtfstate"
+  container_name       = "tfstate"
+  key                  = "aqua.terraform.tfstate"
+}
 
 provider "azurerm" {
   features {
