2b2dbcc2cdfd736cb42552a68ff0722cbed5635a
Matt W <matt.weale@outlook.com>
2022-10-01 08:54:15 +0100

    Moved backend to main.tf

--- main/main.tf
+++ main/main.tf
@@ -10,7 +10,12 @@ terraform {
   }
 
   required_version = ">= 1.3.0"
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-persistent"
+    storage_account_name = "samrwtfstate"
+    container_name       = "tfstate"
+    key                  = "aqua.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
