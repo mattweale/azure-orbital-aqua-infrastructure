a9850fd3bd53d243b037e92fd1e6d6554e3242d3
Matt W <matt.weale@outlook.com>
2022-10-04 09:58:07 +0100

    Moved to dynamic TF backend config

--- main/main.tf
+++ main/main.tf
@@ -10,10 +10,10 @@ terraform {
   }
   required_version = ">= 1.3.0"
   backend "azurerm" {
-    resource_group_name  = "rg-persistent"
-    storage_account_name = "samrwtfstate"
-    container_name       = "tfstate"
-    key                  = "aqua.terraform.tfstate"
+    #resource_group_name  = "rg-persistent"
+    #storage_account_name = "samrwtfstate"
+    #container_name       = "tfstate"
+    #key                  = "aqua.terraform.tfstate"
   }
 }
 provider "azurerm" {
