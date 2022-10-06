f9dee60bc0b5f76276407156b9f566f91057bb83
Matt W <matt.weale@outlook.com>
2022-10-06 19:07:48 +0100

    Changed target zone

--- main/vmDataCollection.tf
+++ main/vmDataCollection.tf
@@ -3,7 +3,6 @@ resource "azurerm_public_ip" "pip_orbital_data_collection" {
   name                = "${local.base_name}-pip-data-collection-vm-${local.suffix}"
   location            = local.rg_location
   resource_group_name = local.rg_name
-  zones               = [2]
   allocation_method   = "Static"
   domain_name_label   = "aqua-data-collection-vm"
   tags                = var.tags
