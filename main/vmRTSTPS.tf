8754b893c95fd10e0e46d972ab6f72e8372fea7a
Matt W <matt.weale@outlook.com>
2022-12-23 15:39:02 +0000

    Updated URI for repo in RT-STPS CSE

--- main/vmRTSTPS.tf
+++ main/vmRTSTPS.tf
@@ -82,11 +82,11 @@ resource "azurerm_virtual_machine_extension" "cse_vm_orbital_rtstps_config" {
   settings = <<SETTINGS
     {
         "commandToExecute":"export AQUA_NFS_SHARE=${azurerm_storage_account.sa_aqua_common.name} && export AQUA_MI_ID=${azurerm_user_assigned_identity.ua_mi_orbital.client_id} && export AQUA_TOOLS_SA=${data.azurerm_storage_account.sa_aqua_tools.name} && ./main_rtstps.sh > ./logfile.txt exit 0",
-        "fileUris":["https://raw.githubusercontent.com/mattweale/azure-orbital-infrastructure/main/vm_configuration/mount_data_drive.sh",
-                    "https://raw.githubusercontent.com/mattweale/azure-orbital-infrastructure/main/vm_configuration/mount_container.sh",
-                    "https://raw.githubusercontent.com/mattweale/azure-orbital-infrastructure/main/vm_configuration/main_rtstps.sh",
-                    "https://raw.githubusercontent.com/mattweale/azure-orbital-infrastructure/main/vm_configuration/install_rtstps.sh",
-                    "https://github.com/mattweale/azure-orbital-infrastructure/blob/main/vm_configuration/aqua_datachecker.py"]
+        "fileUris":["https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/mount_data_drive.sh",
+                    "https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/mount_container.sh",
+                    "https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/main_rtstps.sh",
+                    "https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/install_rtstps.sh",
+                    "https://github.com/mattweale/azure-orbital-aqua-infrastructure/blob/main/vm_configuration/aqua_datachecker.py"]
     }
 SETTINGS
   tags     = var.tags
