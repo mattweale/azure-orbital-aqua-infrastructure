c6106d1c9635cdf036642c8051f2a20ccd63d4cd
Matt W <matt.weale@outlook.com>
2023-01-06 19:03:35 +0000

    Added blobfuse install and cofiguration to VMs

--- main/vmRTSTPS.tf
+++ main/vmRTSTPS.tf
@@ -81,7 +81,7 @@ resource "azurerm_virtual_machine_extension" "cse_vm_orbital_rtstps_config" {
   }
   settings = <<SETTINGS
     {
-        "commandToExecute":"export AQUA_NFS_SHARE=${azurerm_storage_account.sa_aqua_common.name} && export AQUA_MI_ID=${azurerm_user_assigned_identity.ua_mi_orbital.client_id} && export AQUA_TOOLS_SA=${data.azurerm_storage_account.sa_aqua_tools.name} && ./main_rtstps.sh > ./logfile.txt exit 0",
+        "commandToExecute":"export AQUA_NFS_SHARE=${azurerm_storage_account.sa_aqua_common.name} && export T2B_SA_NAME=${data.azurerm_storage_account.sa_data_collection.name} && export AQUA_MI_ID=${azurerm_user_assigned_identity.ua_mi_orbital.client_id} && export AQUA_TOOLS_SA=${data.azurerm_storage_account.sa_aqua_tools.name} && ./main_rtstps.sh > ./logfile.txt exit 0",
         "fileUris":["https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/mount_data_drive.sh",
                     "https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/mount_container.sh",
                     "https://raw.githubusercontent.com/mattweale/azure-orbital-aqua-infrastructure/main/vm_configuration/main_rtstps.sh",
--- vm_configuration/mount_container.sh
+++ vm_configuration/mount_container.sh
@@ -37,3 +37,68 @@
 sudo chmod -R 777 /nfsdata
 
 echo ${AQUA_NFS_SHARE}.blob.core.windows.net:/${AQUA_NFS_SHARE}/shared  /nfsdata    nfs defaults,sec=sys,vers=3,nolock,proto=tcp,nofail    0 0 >> /etc/fstab 
+
+#   Install blobfuse2
+wget https://github.com/Azure/azure-storage-fuse/releases/download/blobfuse2-2.0.1/blobfuse2-2.0.1-CentOS-7.0-x86-64.rpm
+yum install libfuse3-dev fuse3 -y
+rpm -i blobfuse2-2.0.1-CentOS-7.0-x86-64.rpm
+
+#   Create blobfuse mount points, tempcache and config file
+mkdir -p /bf2all
+mkdir /opt/blob-fuse
+mkdir /opt/blob-fuse/tempcache
+chmod -R 777 /opt/blob-fuse
+mkdir -p /bf2all/raw-contact-data
+mkdir -p /bf2all/shared
+chmod -R 777 /bf2all/raw-contact-data
+chmod -R 777 /bf2all/shared
+
+cat <<EOT  >  /opt/blob-fuse/config.yaml
+allow-other: true
+
+logging:
+  type: syslog
+  level: log_debug
+
+components:
+  - libfuse
+  - file_cache
+  - attr_cache
+  - azstorage
+
+libfuse:
+  attribute-expiration-sec: 120
+  entry-expiration-sec: 120
+  negative-entry-expiration-sec: 240
+
+file_cache:
+  path: /opt/blob-fuse/tempcache
+  timeout-sec: 120
+  max-size-mb: 4096
+  cleanup-on-start: true
+  allow-non-empty-temp: true
+
+attr_cache:
+  timeout-sec: 7200
+
+azstorage:
+  type: block
+  account-name: ${T2B_SA_NAME}
+  container: raw-contact-data
+  container: shared
+  endpoint: https://${T2B_SA_NAME}.blob.core.windows.net
+  mode: msi
+  appid: ${AQUA_MI_ID}
+
+health_monitor:
+  enable-monitoring: true
+  stats-poll-interval-sec: 10
+  process-monitor-interval-sec: 30
+  output-path: outputReportsPath
+  monitor-disable-list:
+    - file_cache_monitor
+    - memory_profileraw
+EOT
+
+blobfuse2 mount all /bf2all --config-file=/opt/blob-fuse/config.yaml --allow-other
+
