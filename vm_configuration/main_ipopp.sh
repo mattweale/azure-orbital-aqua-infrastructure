c1d4fc545fc6ad0b14884a9eb8a637b7ae1f3def
Matt W <matt.weale@outlook.com>
2023-01-08 09:55:24 +0000

    Updated main_ipopp.sh

--- vm_configuration/main_ipopp.sh
+++ vm_configuration/main_ipopp.sh
@@ -2,5 +2,5 @@
 
 sudo ./mount_data_drive.sh
 sudo ./mount_container.sh
-sudo -E bash -c ./ipopp_prereqs.sh
+sudo -E bash -c ./prereqs_ipopp.sh
 su -c "/datadrive/install_ipopp.sh" -s /bin/bash adminuser
